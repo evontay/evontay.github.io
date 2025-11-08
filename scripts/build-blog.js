7043b572507080492934add4a3b586333bad33b5
EVON TAY <fumblies@gmail.com>
2025-11-08 17:07:55 +0800

    Update build-blog.js
    Updated css file location

--- scripts/build-blog.js
+++ scripts/build-blog.js
@@ -19,7 +19,7 @@ const blogTemplate = (title, date, content, excerpt) => `<!DOCTYPE html>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="${excerpt}">
     <title>${title} - Evon Tay</title>
-    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../app.css">
 </head>
 <body>
     <header>
